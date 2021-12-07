266614f8d636306b40af17eac394cc2ede0293be
Alwin <nedrum@gmail.com>
2021-12-07 14:32:36 +0100

    set up components

---
+++ src/Input.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let label: string
+  export let max: number
+  console.log(max)
+
+
+</script>
+
+<label>
+    {label}
+    <input type="text" {max}/>
+</label>
+
+
+
+
+<style>
+
+
+
+
+
+</style>
