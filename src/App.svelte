0779806a22e6e80389d824dd757838f40e02e81b
Alwin <nedrum@gmail.com>
2021-12-07 14:33:42 +0100

    container Component can be changed from props

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+import Container from "./Container.svelte";
+
+	import Input from "./Input.svelte";
+	
+</script>
+
+<Input max={7} label="hello number"/>
+
+<Container id="first" color="salmon">
+</Container>
+
+<Container id="second" color="cyan">
+</Container>
+
+<Container id="third" color="goldenrod">
+</Container>
+
+
+
+<style>
+
+</style>
---
+++ src/Container.svelte
@@ -0,0 +1,42 @@
+<script lang="ts" >
+    import {onMount} from 'svelte'
+      export let color :string = "cyan" || "samon" || "goldenrod"
+      export let id: string
+     let wrapper
+     
+      let queryString = "#"+id
+     
+      
+onMount( () => {
+  let stuff = document.querySelector(queryString)
+    stuff.classList.add(color)
+   console.log(id, stuff, color);
+  })
+   
+   
+   
+      
+</script>
+
+
+<div bind:this="{wrapper}" {id} class="container {color}"  {color}>
+<slot/>
+
+</div>
+
+
+<style>
+.container {
+    margin: 1em;
+    /* background-color: blue; */
+    width: 300px;
+    height: 300px;
+
+}
+
+.cyan {background-color: cyan;}
+.salmon {background-color: salmon;}
+.goldenrod {background-color: goldenrod;}
+
+
+</style>
